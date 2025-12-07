cf7ff6955f3e4340b80bd828c811f1b84ad64949
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2025-12-07 12:36:07 +0500

    Initialized

---
+++ app.js
@@ -0,0 +1,51 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const cookieParser = require('cookie-parser');
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
+const mapsRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
+
+connectToDb();
+
+app.use(
+  cors({
+    origin: [
+      "https://j9r4m1mn-5173.asse.devtunnels.ms",
+      "http://localhost:5173",
+
+
+    ],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization",
+        
+      
+    ],
+    credentials:true
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+
+app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
+
+
+
+
+module.exports = app;
+
